fe85283aa0719f0edce7be801647fe5bbd310f9d
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2022-11-11 16:56:47 -0300

    criar o dataframe do texto e suas classes

--- teste.py
+++ teste.py
@@ -1,13 +1,38 @@
 import wikipedia as wiki
 import re 
+import pandas as pd
+
+import nltk
+from nltk import tokenize  
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+nltk.download('stopwords')
+stopwords = nltk.corpus.stopwords.words('portuguese')
+print(stopwords)
 
 wiki.set_lang("pt")
-breast_cancer_content = wiki.page("Breast_cancer").content
+breast_cancer_content = wiki.page("Breast cancer").content
 
 classes = re.findall('=\s(.*)\s=',breast_cancer_content)
-sentences = re.sub(r'=(.*)=', '',breast_cancer_content)
-sentences = re.split(r'\n', sentences)
-while '' in sentences:
-    sentences.remove('')
+raw = re.split(r'=+\s(.*)\s=+',breast_cancer_content)
 
-print(sentences)
+blocos = []
+for r in range(0, len(raw)-1, 2):
+    blocos.append(raw[r])
+
+classes.insert(0, 'Cancer de mama')
+
+d = {'classes': [], 'sentencas':[]}
+df = pd.DataFrame(d)
+
+for b in range(0, len(blocos)-1):
+    sentences = tokenize.sent_tokenize(blocos[b], language='portuguese')
+    for s in sentences:
+        s = re.sub(r'\n', '', s)
+        w = tokenize.word_tokenize(s)
+        df = pd.concat([df, pd.Series({'classes': classes[b], 'sentencas': w}).to_frame().T], ignore_index=True) 
+
+# for i, line in df.iterrows():
+    # print(line['classes'], line['sentencas'])
+print(df.head())
